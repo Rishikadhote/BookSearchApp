bc01106b964c2ed6923a0dbd7f44abbcd7398355
Rishikadhote <rishikadhote99@gmail.com>
2025-03-24 05:14:49 +0530

    commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,8 +16,7 @@ const About = () => {
           </div>
           <div className='about-text'>
             <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dignissimos consequuntur vero commodi provident maiores, iusto atque corrupti voluptate vel sequi consectetur unde aliquam corporis saepe animi non, tempora reiciendis molestias sed laudantium dolores. Assumenda aperiam fuga quo voluptate commodi ullam aliquam expedita voluptas delectus.</p>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+            <p className='fs-17'>At Bookhouse, every book is a gateway to a world of discovery and inspiration. We know that literature has the power to change and enrich lives, inspire creativity and imagination, and provide a sanctuary for inquiring minds. From timeless classics to modern bestsellers and buried treasures, our selection caters to spark your imagination and expand your love affair with reading. More than a book collection, Bookhouse is your own library—a space where you can maintain a review journal, monitor your literary journey, and hold dear each unforgettable experience. Here, each page turned becomes an invitation to discover new ideas and ponder the stories that transform you. Welcome to Bookhouse, where your own reading experience opens up into infinite possibilities.</p>
           </div>
         </div>
       </div>
